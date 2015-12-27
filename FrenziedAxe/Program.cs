e9572da4fa3d001015678b54b7dbfa56c266985c
Montroz <Montrozm@gmail.com>
2015-12-28 02:52:14 +0300

    axe

---
+++ FrenziedAxe/Program.cs
@@ -0,0 +1,45 @@
+﻿using Ensage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenziedAxe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Game.IsInGame)
+            {
+                return;
+            }
+
+            Hero me = ObjectMgr.LocalHero;
+
+            if (me == null || me.ClassID != ClassID.CDOTA_Unit_Hero_Axe)
+            {
+                return;
+            }
+            else {
+                Game.OnUpdate += Game_OnUpdate;
+            }
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (!Game.IsInGame)
+            {
+                return;
+            }
+
+            Hero me = ObjectMgr.LocalHero;
+
+            if (me == null || me.ClassID != ClassID.CDOTA_Unit_Hero_Axe)
+            {
+                return;
+            }
+        }
+    }
+}
